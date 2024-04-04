12c39877931506d5844ca9308209426abb9ceb5f
Userman1986 <e.samonin1986@gmail.com>
2024-04-04 21:26:04 +0200

    Revert "Update Chat.js"
    This reverts commit af9aabd809131bce5bbc21a109bfb6edf7f8bb7c.

--- components/Chat.js
+++ components/Chat.js
@@ -8,20 +8,15 @@ const Chat = ({ route }) => {
   // State to manage chat messages
   const [messages, setMessages] = useState([]);
 
+  // useEffect to initialize chat messages
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello developer",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://placeimg.com/140/140/any",
-        },
-      },
-    ]);
-  }, []);
+    // Adding initial system message
+    setMessages(previousMessages => GiftedChat.append(previousMessages, {
+      _id: 1,
+      text: `Welcome, ${name}! You've entered the chat.`,
+      createdAt: new Date(),
+      system: true,
+    }));
 
     // Adding initial user message
     setMessages(previousMessages => GiftedChat.append(previousMessages, {
